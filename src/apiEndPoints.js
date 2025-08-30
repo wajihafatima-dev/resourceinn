d3e588db8c053c7fd8ff022fbb22034a7a84402c
wajihafatima-dev <wajihafatima689@gmail.com>
2025-08-30 17:07:34 +0500

    add env base url

--- src/apiEndPoints.js
+++ src/apiEndPoints.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://resourceinn.vercel.app";
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const loginApi = "/api/login";
 export const signupApi = "/api/signup";
 export const cardApi = "/api/cards";
